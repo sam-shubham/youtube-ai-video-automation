af047790179b323f30cfcd17d1acc4e10ab669ce
Sam Shubham <68236217+sam-shubham@users.noreply.github.com>
2025-06-03 03:37:09 +0530

    added docker file for rende

--- server.js
+++ server.js
@@ -353,8 +353,17 @@ async function generateCodeSnippetWithRayso(
     console.log("Ray.so URL:", raysoUrl);
 
     const browser = await puppeteer.launch({
-      headless: "new",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
